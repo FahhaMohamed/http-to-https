999de574b19a602b62784383892dbc4d22184647
Mohamed Fahham <fahhamohmad17@gmail.com>
2025-07-20 13:54:27 +0530

    added post method

--- http_server.js
+++ http_server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.status(200).json({
     status: true,
     message: "Response sent from HTTP Server!",
+  });
+});
+
+app.post("/", (req, res) => {
+  const { username } = req.body;
+  res.status(200).json({
+    status: true,
+    message: `Hello, ${username}! Welcome to the HTTP Server!`,
   });
 });
 
--- https_server.js
+++ https_server.js
@@ -3,6 +3,8 @@ const https = require("https");
 const selfSignedCert = require("selfsigned");
 const app = express();
 
+app.use(express.json());
+
 const attributes = [{ name: "commonName", value: "localhost" }];
 const ssl = selfSignedCert.generate(attributes, { days: 365 });
 
@@ -17,6 +19,14 @@ app.get("/", (req, res) => {
   res.status(200).json({
     status: true,
     message: "Response sent from HTTPS Server!"
+  });
+});
+
+app.post("/", (req, res) => {
+  const { username } = req.body;
+  res.status(200).json({
+    status: true,
+    message: `Hello, ${username}! Welcome to the HTTPS Server!`,
   });
 });
 
